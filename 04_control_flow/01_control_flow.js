e381260ff25dd5d114dc6d5069c65940dfa873fd
Mangesh_Mali_4038 <mangeshmali2002@gmail.com>
2024-02-10 14:29:38 +0530

    Add files via upload

---
+++ 04_control_flow/01_control_flow.js
@@ -0,0 +1,20 @@
+// cntrol statements are used to control the flow of the program 
+if(true){
+    
+}
+
+
+// else if
+// switch case
+// ternary operator 
+// null scheing 
+
+
+
+
+//falsy values 
+// false, 0, -0, NAN, NULL, undefined, BigInt 0n, ""
+
+
+// Truth values 
+// "0", 'false', " ", [], {}, function(){}
